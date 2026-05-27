a5a592b559eae57b7a67809e863080fafea2570a
MTVirux <joaofraga96@outlook.com>
2026-05-27 09:00:07 +0100

    Add Job Roulette configuration options

--- TerrorTweaks/Configuration.cs
+++ TerrorTweaks/Configuration.cs
@@ -11,5 +11,15 @@ public sealed class Configuration : IPluginConfiguration
 
     public HashSet<string> EnabledTweaks { get; set; } = [];
 
+    public JobRouletteConfig JobRoulette { get; set; } = new();
+
     public void Save() => Services.PluginInterface.SavePluginConfig(this);
+}
+
+[Serializable]
+public sealed class JobRouletteConfig
+{
+    public bool IncludeCrafters { get; set; }
+    public bool IncludeGatherers { get; set; }
+    public bool IncludeLimited { get; set; }
 }

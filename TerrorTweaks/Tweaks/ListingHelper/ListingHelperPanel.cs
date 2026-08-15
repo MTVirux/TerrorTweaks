7a70e494c910495113923c8a9700f105f987affa
MTVirux <joaofraga96@outlook.com>
2026-08-15 05:23:21 +0100

    Let the listing helper columns be resized

--- TerrorTweaks/Tweaks/ListingHelper/ListingHelperPanel.cs
+++ TerrorTweaks/Tweaks/ListingHelper/ListingHelperPanel.cs
@@ -141,7 +141,8 @@ internal sealed class ListingHelperPanel
 
     private void DrawTable(List<PanelRow> rows)
     {
-        const ImGuiTableFlags flags = ImGuiTableFlags.RowBg | ImGuiTableFlags.BordersInnerH | ImGuiTableFlags.ScrollY;
+        const ImGuiTableFlags flags = ImGuiTableFlags.RowBg | ImGuiTableFlags.BordersInnerH
+            | ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable;
         // Whatever the table does not take is what the separator and button row get, so this is
         // the one place the height of the strip along the bottom is set.
         var height = ImGui.GetContentRegionAvail().Y - ImGui.GetFrameHeightWithSpacing();
